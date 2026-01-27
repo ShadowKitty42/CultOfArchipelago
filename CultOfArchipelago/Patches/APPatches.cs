52718859485b70c540f9b9429cafed12acc91127
ShadowKitty42 <shadowxdragonx42@gmail.com>
2026-01-26 18:28:18 -0800

    More attempts at patching. Added project for Archipelago Fork

--- CultOfArchipelago/Patches/APPatches.cs
+++ CultOfArchipelago/Patches/APPatches.cs
@@ -7,41 +7,44 @@ using HarmonyLib;
 using Lamb.UI;
 using Lamb.UI.UpgradeMenu;
 
-namespace CultOfArchipelago.Patches
+namespace CultOfArchipelago
 {
-    [HarmonyPatch(typeof(UIUpgradeShopMenuController), nameof(UIUpgradeShopMenuController.OnEnable))]
-    public static class APPatches
+    public class Patches
     {
-        public static void Prefix(UIUpgradeShopMenuController __instance)
+        /*[HarmonyPatch(typeof(UIUpgradeShopMenuController), nameof(UIUpgradeShopMenuController.OnEnable))]
+        public static class APPatches
         {
-            // Example patch logic
-            Plugin.Logger.LogInfo("UIUpgradeShopMenuController OnShowStarted called.");
-            foreach(UIUpgradeShopMenuController.AvailableUpgrades upgrade in __instance._upgrades)
+            public static void Prefix(UIUpgradeShopMenuController __instance)
             {
-                Plugin.Logger.LogInfo($"Available Upgrade: {upgrade.Type}, {upgrade.RequireUnlockedType}, {upgrade.RequireUnlocked}, {upgrade.CheckUnlocked}");
+                // Example patch logic
+                Plugin.Logger.LogInfo("UIUpgradeShopMenuController OnShowStarted called.");
+                foreach(UIUpgradeShopMenuController.AvailableUpgrades upgrade in __instance._upgrades)
+                {
+                    Plugin.Logger.LogInfo($"Available Upgrade: {upgrade.Type}, {upgrade.RequireUnlockedType}, {upgrade.RequireUnlocked}, {upgrade.CheckUnlocked}");
+                }
+            }
+        }*/
+
+        [HarmonyPatch(typeof(UpgradeTreeNode), nameof(UpgradeTreeNode.IsAvailable))]
+        public static class UpgradeTree
+        {
+            public static bool Prefix(ref bool __result)
+            {
+                Plugin.Logger.LogInfo($"UpgradeTreeNode Prefix was hit!");
+                __result = true;
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(UIUpgradeUnlockOverlayController), nameof(UIUpgradeUnlockOverlayController.IsAvailable))]
+        public static class UpgradeUnlockOverlayControllerPatch
+        {
+            public static bool Prefix(ref bool __result)
+            {
+                //__result = true;
+                Plugin.Logger.LogInfo($"UIUpgradeUnlockOverlayController IsAvailable Prefix was hit!");
+                return true;
             }
         }
     }
-
-    /*[HarmonyPatch(typeof(UpgradeTreeNode), nameof(UpgradeTreeNode.IsAvailable))]
-    public static class UpgradeTree
-    {
-        public static bool Prefix(ref bool __result)
-        {
-            Plugin.Logger.LogInfo($"UpgradeTreeNode Prefix was hit!");
-            __result = true;
-            return false;
-        }
-    }
-
-    [HarmonyPatch(typeof(UIUpgradeUnlockOverlayController), nameof(UIUpgradeUnlockOverlayController.IsAvailable))]
-    public static class UpgradeUnlockOverlayControllerPatch
-    {
-        public static bool Prefix(ref bool __result)
-        {
-            //__result = true;
-            Plugin.Logger.LogInfo($"UIUpgradeUnlockOverlayController IsAvailable Prefix was hit!");
-            return true;
-        }
-    }*/
 }
--- CultOfArchipelago/Plugin.cs
+++ CultOfArchipelago/Plugin.cs
@@ -8,14 +8,14 @@ namespace CultOfArchipelago
     public class Plugin : BaseUnityPlugin
     {
         internal static new ManualLogSource Logger;
-        private static readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        private static Harmony harmony;
 
         private void Awake()
         {
             // Plugin startup logic
             Logger = base.Logger;
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-            harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+            harmony = Harmony.CreateAndPatchAll(typeof(Patches));
         }
 
         private void OnDestroy()
